225c20d52a67d7b48325d7650c97ce49b454797b
Thomas Cheng <thomascheng1998@googlemail.com>
2017-05-21 20:58:17 +0100

    Created realtime fluid sim from the gdc_03 paper on approximating the solutions to the navier stokes equations. Currently borders working incorrectly, and it runs slowly.

---
+++ src/fluid.rs
@@ -0,0 +1,205 @@
+#![allow(dead_code)]
+
+/// Macro for indexing into a 1D array using 2D coordinates.
+macro_rules! IX {
+  ( $x: expr, $y: expr,  $w: expr ) => {
+    { $x as usize + $y as usize * $w as usize }
+  }
+}
+
+/// A source of fluid.
+pub struct Source {
+  /// The index of the source in the grid array
+  pub ix: usize,
+  /// Value between 0 and 1, density of the fluid at source.
+  pub density: f32,
+}
+
+impl Source {
+  /// Create a new source, given an index into a float array for its position.
+  pub fn new(ix: usize, density: f32) -> Source {
+    Source { ix: ix, density: density }
+  }
+}
+
+/// Add borders to the density grid. We can do this by just setting the density
+/// of the outer cells to be equal to the density of the cells inset by 1. This
+/// means the difference between the two cells is 0, so there will never be any
+/// flow outwards.
+/// # Params
+/// * `b` - The type of border. 1 for vertical vel walls, 2 for hori vel walls, 0 for dens.
+fn set_borders(grid: &mut [f32], grid_w: u32, b: u8) {
+  let grid_h = grid.len()/grid_w as usize;
+  for ii in 0..grid_w {
+    let ix_top = IX!(ii, 0, grid_w);
+    let ix_top_inset = IX!(ii, 1, grid_w);
+    let ix_bot = IX!(ii, grid_h-1, grid_w);
+    let ix_bot_inset = IX!(ii, grid_h-2, grid_w);
+    grid[ix_top] = {if b == 2 {-grid[ix_top_inset]} else {grid[ix_top_inset]}};
+    grid[ix_bot] = {if b == 2 {-grid[ix_bot_inset]} else {grid[ix_bot_inset]}};
+  }
+  for ii in 0..grid_h {
+    let ix_left = IX!(0, ii, grid_w);
+    let ix_left_inset = IX!(1, ii, grid_w);
+    let ix_right = IX!(grid_w-1, ii, grid_w);
+    let ix_right_inset = IX!(grid_w-2, ii, grid_w);
+    grid[ix_left] = {if b == 1 {-grid[ix_left_inset]} else {grid[ix_left_inset]}};
+    grid[ix_right] = {if b == 1 {-grid[ix_right_inset]} else {grid[ix_right_inset]}};
+  }
+  grid[IX!(0, 0, grid_w)]             = 0.5*(grid[IX!(1, 0, grid_w)]+grid[IX!(0, 1, grid_w)]);
+  grid[IX!(0, grid_h-1, grid_w)]      = 0.5*(grid[IX!(1, grid_h-1, grid_w)]+grid[IX!(0, grid_h-2, grid_w)]);
+  grid[IX!(grid_w-1, 0, grid_w)]      = 0.5*(grid[IX!(grid_w-2, 0, grid_w)]+grid[IX!(grid_w-1, 1, grid_w)]);
+  grid[IX!(grid_w-1, grid_h-1, grid_w)] = 0.5*(grid[IX!(grid_w-2, grid_h-1, grid_w)]+grid[IX!(grid_w-1, grid_h-2, grid_w)]);
+}
+
+/// Process diffusion
+/// # Params
+/// * `b` - Border type, see set_borders
+fn diffuse(grid: &mut [f32], prev_grid: &mut [f32], grid_w: u32, dt: f32, diff: f32, borders: bool, b: u8) {
+  let a = dt*diff*grid.len() as f32;
+  let grid_h = grid.len()/grid_w as usize;
+  let iterations = 1;
+  // Iteratively diffuse, solve the linear system to find the next state of the grid
+  for _ in 0..iterations {
+    for ii in 1..grid_w-1 {
+      for jj in 1..grid_h-1 {
+        let ix = IX!(ii, jj, grid_w);
+        let ix_up = IX!(ii, jj-1, grid_w); // 1 row up
+        let ix_down = IX!(ii, jj+1, grid_w); // 1 row down
+        grid[ix] = (prev_grid[ix] + a*(grid[ix-1] + grid[ix+1] 
+                                       + grid[ix_up] + grid[ix_down]))/(1.0+4.0*a);
+      }
+    }
+    if borders { set_borders(grid, grid_w, b) }
+  }
+}
+
+/// Process density movement via velocity
+/// # Params
+/// * `b` - Border type, see set_borders
+fn advect(grid: &mut [f32], prev_grid: &[f32], vx_grid: &[f32], vy_grid: &[f32], grid_w: u32, dt: f32, borders: bool, b: u8) {
+  let grid_h = grid.len()/grid_w as usize;
+  let dt0 = dt*grid_w as f32;
+
+  for ii in 1..grid_w-1 {
+    for jj in 1..grid_h-1 {
+      let ix = IX!(ii, jj, grid_w);
+      let mut x = ii as f32 - dt0 * vx_grid[ix];
+      let mut y = jj as f32 - dt0 * vy_grid[ix];
+
+      if x < 0.5 { x = 0.5 }
+      if x > grid_w as f32 - 1.5 { x = grid_w as f32 - 1.5 }
+      let ii0 = x as u32;
+      let ii1 = ii0 + 1;
+      if y < 0.5 { y = 0.5 }
+      if y > grid_h as f32 - 1.5 { y = grid_h as f32 - 1.5 }
+      let jj0 = y as u32;
+      let jj1 = jj0 + 1;
+
+      let s1 = x - ii0 as f32;
+      let s0 = 1.0 - s1;
+      let t1 = y - jj0 as f32;
+      let t0 = 1.0 - t1;
+
+      let ix00 = IX!(ii0, jj0, grid_w);
+      let ix01 = IX!(ii0, jj1, grid_w);
+      let ix11 = IX!(ii1, jj1, grid_w);
+      let ix10 = IX!(ii1, jj0, grid_w);
+      grid[ix] = s0 * (t0*prev_grid[ix00]+t1*prev_grid[ix01])
+        + s1 * (t0*prev_grid[ix10] + t1*prev_grid[ix11]);
+    }
+  }
+  if borders { set_borders(grid, grid_w, b); }
+}
+
+fn project(vx_grid: &mut [f32], vy_grid: &mut [f32], p: &mut [f32], div: &mut [f32], grid_w: u32, borders: bool) {
+  let h = 1.0/grid_w as f32;
+  let grid_h = vx_grid.len()/grid_w as usize;
+  for ii in 1..grid_w-1 {
+    for jj in 1..grid_h-1 {
+      let ix = IX!(ii, jj, grid_w);
+      let ix_up = IX!(ii, jj-1, grid_w); // 1 row up
+      let ix_down = IX!(ii, jj+1, grid_w); // 1 row down
+      div[ix] = -0.5*h*(vx_grid[ix+1]-vx_grid[ix-1] +
+                        vy_grid[ix_down]-vy_grid[ix_up]);
+      p[ix] = 0.0;
+    }
+  }
+  if borders { set_borders(div, grid_w, 0); set_borders(p, grid_w, 0) }
+
+  let iterations = 1;
+  for _ in 0..iterations {
+    for ii in 1..grid_w-1 {
+      for jj in 1..grid_h-1 {
+        let ix = IX!(ii, jj, grid_w);
+        let ix_up = IX!(ii, jj-1, grid_w); // 1 row up
+        let ix_down = IX!(ii, jj+1, grid_w); // 1 row down
+        p[ix] = (div[ix] + p[ix-1] + p[ix+1] + p[ix_up] + p[ix_down])/4.0;
+      }
+    }
+    if borders { set_borders(p, grid_w, 0) }
+  }
+
+  for ii in 1..grid_w-1 {
+    for jj in 1..grid_h-1 {
+      let ix = IX!(ii, jj, grid_w);
+      let ix_up = IX!(ii, jj-1, grid_w); // 1 row up
+      let ix_down = IX!(ii, jj+1, grid_w); // 1 row down
+      vx_grid[ix] -= 0.5*(p[ix+1]-p[ix-1])/h;
+      vy_grid[ix] -= 0.5*(p[ix_down]-p[ix_up])/h;
+    }
+  }
+  if borders { set_borders(vx_grid, grid_w, 1); set_borders(vy_grid, grid_w, 2) }
+}
+
+/// Step density
+fn step_dens(dens_grid: &mut [f32], vx_grid: &[f32], vy_grid: &[f32], grid_w: u32, dt: f32, diff: f32, borders: bool) {
+  // Make a copy of the dens_grid
+  let mut prev_dens_grid = dens_grid.to_vec();
+  let prev_dens_grid = &mut prev_dens_grid[..];
+
+  // Swap binding in preparation for the next swap
+  let (prev_dens_grid, dens_grid) = (dens_grid, prev_dens_grid);
+
+  // Process diffusion
+  diffuse(dens_grid, prev_dens_grid, grid_w, dt, diff, borders, 0);
+
+  // Swap bindings, b/c we just updates dens_grid and advect() needs to use
+  // that for the previous grid
+  let (prev_dens_grid, dens_grid) = (dens_grid, prev_dens_grid);
+
+  // Process velocity of particles
+  advect(dens_grid, prev_dens_grid, vx_grid, vy_grid, grid_w, dt, borders, 0);
+}
+
+/// Step velocity
+fn step_vel(vx_grid: &mut [f32], vy_grid: &mut [f32], grid_w: u32, dt: f32, diff: f32, borders: bool) {
+  // Copy velocity grids, so we have a copy of it before processing each step
+  let mut prev_vx_grid = vx_grid.to_vec();
+  let mut prev_vy_grid = vy_grid.to_vec();
+  let prev_vx_grid = &mut prev_vx_grid[..];
+  let prev_vy_grid = &mut prev_vy_grid[..];
+
+  let (prev_vx_grid, vx_grid) = (vx_grid, prev_vx_grid);
+  let (prev_vy_grid, vy_grid) = (vy_grid, prev_vy_grid);
+
+  // Diffuse just like with density but with velocity instead
+  diffuse(vx_grid, prev_vx_grid, grid_w, dt, diff, borders, 1);
+  diffuse(vy_grid, prev_vy_grid, grid_w, dt, diff, borders, 2);
+  project(vx_grid, vy_grid, prev_vx_grid, prev_vy_grid, grid_w, borders);
+
+  let (prev_vx_grid, vx_grid) = (vx_grid, prev_vx_grid);
+  let (prev_vy_grid, vy_grid) = (vy_grid, prev_vy_grid);
+
+  // Advect just like with density
+  advect(vx_grid, prev_vx_grid, prev_vx_grid, prev_vy_grid, grid_w, dt, borders, 1);
+  advect(vy_grid, prev_vy_grid, prev_vx_grid, prev_vy_grid, grid_w, dt, borders, 2);
+  project(vx_grid, vy_grid, prev_vx_grid, prev_vy_grid, grid_w, borders);
+}
+
+/// Steps a grid of floats containing fluid data according to dt.
+pub fn step_fluid(dens_grid: &mut [f32], vx_grid: &mut [f32], vy_grid: &mut [f32], grid_w: u32, dt: f32, diff: f32, borders: bool) {
+  // Step density, alter density grid
+  step_dens(dens_grid, vx_grid, vy_grid, grid_w, dt, diff, borders);
+  step_vel(vx_grid, vy_grid, grid_w, dt, diff, borders);
+}
---
+++ src/main.rs
@@ -0,0 +1,147 @@
+#[macro_use]
+extern crate glium;
+
+mod fluid;
+
+use glium::backend::glutin_backend::GlutinFacade;
+
+#[derive(Copy, Clone)]
+struct Vertex {
+    pos: [f32; 2],
+    uv: [f32; 2],
+}
+implement_vertex!(Vertex, pos, uv);
+
+static VERT_SRC : &'static str = r#"
+    #version 140
+
+    in vec2 pos;
+    in vec2 uv;
+
+    out vec2 v_uv;
+
+    void main() {
+      v_uv = uv;
+      gl_Position = vec4(pos, 0.0, 1.0);
+    }
+"#;
+
+static FRAG_SRC : &'static str = r#"
+    #version 140
+
+    uniform highp sampler2D tex;
+    uniform highp sampler2D tex1;
+    uniform highp sampler2D tex2;
+
+    in vec2 v_uv;
+
+    out vec4 color;
+
+    void main() {
+      color = vec4(texture(tex, v_uv).x, texture(tex1, v_uv).x, texture(tex2, v_uv).x, 1.0);
+    }
+"#;
+
+
+fn setup_display() -> GlutinFacade {
+  use glium::DisplayBuild;
+  glium::glutin::WindowBuilder::new().build_glium().unwrap()
+}
+
+fn setup_shader(display: &GlutinFacade) -> glium::Program {
+  glium::Program::from_source(display, VERT_SRC, FRAG_SRC, None).unwrap()
+}
+
+fn main() {
+  let display = setup_display();
+  let shader = setup_shader(&display);
+  let (display_w, display_h) = display.get_window().unwrap().get_inner_size().unwrap();
+
+  let vbo_data = vec![
+    Vertex{ pos: [-1.0, -1.0], uv: [0.0, 0.0] },
+    Vertex{ pos: [ 1.0, -1.0], uv: [1.0, 0.0] },
+    Vertex{ pos: [ 1.0,  1.0], uv: [1.0, 1.0] },
+    Vertex{ pos: [-1.0, -1.0], uv: [0.0, 0.0] },
+    Vertex{ pos: [-1.0,  1.0], uv: [0.0, 1.0] },
+    Vertex{ pos: [ 1.0,  1.0], uv: [1.0, 1.0] }, ];
+
+
+  let vbo = glium::VertexBuffer::new(&display, &vbo_data).unwrap();
+  let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+
+  // Set up fluids
+
+  const TEX_SIZE: usize = 256;
+  // Velocity grids
+  let mut vx_grid = [0.0; TEX_SIZE*TEX_SIZE].to_vec();
+  let mut vy_grid = [0.0; TEX_SIZE*TEX_SIZE].to_vec();
+  let mut tex_data = [0.0; TEX_SIZE*TEX_SIZE].to_vec();
+  // Create texture data buffer for fluid
+
+  let (mut prev_mx, mut prev_my) = (0, 0);
+  loop {
+    // listing the events produced by the window and waiting to be received
+    for ev in display.poll_events() {
+      match ev {
+        glium::glutin::Event::Closed => { return }   // the window has been closed by the user
+        glium::glutin::Event::MouseMoved(x, y) => {
+          let x = (x as f32 / display_w as f32)*TEX_SIZE as f32;
+          let y = ((display_h as f32 - y as f32) / display_h as f32)*TEX_SIZE as f32;
+          let (x, y) = (x as usize, y as usize);
+          let ix = x + y*TEX_SIZE;
+          if ix >= tex_data.len() { continue; }
+          tex_data[x + y*TEX_SIZE] = 1.0;
+          if prev_mx != 0 && prev_my != 0 {
+            vx_grid[x + y*TEX_SIZE] = (x as f32 - prev_mx as f32)*2000.0;
+            vy_grid[x + y*TEX_SIZE] = (y as f32 - prev_my as f32)*2000.0;
+          }
+          prev_mx = x;
+          prev_my = y;
+        }
+        _ => ()
+      }
+    }
+
+    // Process fluids
+    fluid::step_fluid(&mut tex_data[..], &mut vx_grid[..], &mut vy_grid[..], TEX_SIZE as u32, 0.016, 0.0001, true);
+
+    // Re buffer texture
+    use std::borrow::Cow;
+    let raw_tex_2d = glium::texture::RawImage2d {
+      data: Cow::from(tex_data.clone()),
+      width: TEX_SIZE as u32, height: TEX_SIZE as u32,
+      format: glium::texture::ClientFormat::F32, };
+    let raw_tex_2d1 = glium::texture::RawImage2d {
+      data: Cow::from(vx_grid.clone()),
+      width: TEX_SIZE as u32, height: TEX_SIZE as u32,
+      format: glium::texture::ClientFormat::F32, };
+    let raw_tex_2d2 = glium::texture::RawImage2d {
+      data: Cow::from(vy_grid.clone()),
+      width: TEX_SIZE as u32, height: TEX_SIZE as u32,
+      format: glium::texture::ClientFormat::F32, };
+    let texture = glium::texture::Texture2d::new(&display, raw_tex_2d).unwrap();
+    let texture1 = glium::texture::Texture2d::new(&display, raw_tex_2d1).unwrap();
+    let texture2 = glium::texture::Texture2d::new(&display, raw_tex_2d2).unwrap();
+
+    // Load texture into uniforms
+    let uniforms = uniform! { 
+      tex: texture.sampled()
+        .wrap_function(glium::uniforms::SamplerWrapFunction::Clamp)
+        .magnify_filter(glium::uniforms::MagnifySamplerFilter::Linear),
+      tex1: texture1.sampled()
+        .wrap_function(glium::uniforms::SamplerWrapFunction::Clamp)
+        .magnify_filter(glium::uniforms::MagnifySamplerFilter::Linear),
+      tex2: texture2.sampled()
+        .wrap_function(glium::uniforms::SamplerWrapFunction::Clamp)
+        .magnify_filter(glium::uniforms::MagnifySamplerFilter::Linear)
+    };
+
+    // Draw
+    use glium::Surface;
+    let mut target = display.draw();
+    target.clear_color(0.0, 0.0, 1.0, 1.0);
+    target.draw(&vbo, &indices, &shader, &uniforms, &Default::default()).unwrap();
+    target.finish().unwrap();
+  }
+}
